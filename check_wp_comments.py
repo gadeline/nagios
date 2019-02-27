55a069515b6a20460a239ea1a60484ccd47e74d2
gadeline <46686111+gadeline@users.noreply.github.com>
2019-02-27 22:59:27 +0100

    Add files via upload

---
+++ check_wp_comments.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2.7
+
+import sys
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="nagios_read",
+  passwd="N@Gios01!",
+  database="wordpress_db"
+)
+
+cursor = mydb.cursor()
+
+
+cursor.execute("""
+SELECT count(*) as result FROM wp_comments where comment_date between DATE_SUB(NOW(), INTERVAL 4 HOUR) and now();
+	""")
+
+row = cursor.fetchone()
+print(row[0])
+
+count = row[0]
+
+if count < 4:
+	print("OK")
+	sys.exit(0)
+elif count >= 4 and count < 10:
+	print("WARNING")
+	sys.exit(1)
+elif count >= 10:
+	print("CRITICAL")
+	sys.exit(2)
+else:
+	print("UNKNOW")
+	sys.exit(3)
+
+
